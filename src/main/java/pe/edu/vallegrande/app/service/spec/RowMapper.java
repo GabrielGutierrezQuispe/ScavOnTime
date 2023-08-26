e00b2994b52e030da19d1171a01636c572f8657d
AlexCahuanaValentin <gabriel.gutierrezq@vallegrande.edu.pe>
2023-08-26 09:02:32 -0500

    .

--- src/main/java/pe/edu/vallegrande/app/service/spec/RowMapper.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.vallegrande.app.service.spec;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-public interface RowMapper<T> {
-
-	T mapRow(ResultSet rs) throws SQLException;
-}
--- src/main/webapp/js/assistanceEducationalStaff.js
+++
@@ -1,2 +0,0 @@
-//Funcion para listar profesores
-
